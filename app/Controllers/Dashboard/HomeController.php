b9c9081413480319225606ea1e13ffc295806162
Aryo Bimo <abimo2018@gmail.com>
2023-11-21 19:25:03 +0700

    Fix form

--- app/Controllers/Dashboard/HomeController.php
+++ app/Controllers/Dashboard/HomeController.php
@@ -3,13 +3,17 @@
 namespace App\Controllers\Dashboard;
 
 use App\Controllers\BaseController;
+use App\Models\ReportModel;
 
 class HomeController extends BaseController
 {
     public function index()
     {
         helper('form', 'form_helper');
+        $report = new ReportModel();
         $data["months"] = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+        $data["januari"] = $report->select('id_kec,id_desa,bulan,luas')->selectMax('minggu')->groupBy('id_kec,id_desa,bulan, luas, tipe_komoditas')->find();
+        // dd($data);
         return view('dashboard/index', $data);
     }
 }
--- app/Controllers/Report.php
+++ app/Controllers/Report.php
@@ -17,12 +17,10 @@ class Report extends BaseController
         $kelurahans = new KelurahanModel();
         $data["name"] = session()->get('name');
         $kec_id = session()->get('id_kec');
-        $data["kecamatan"] = $kecamatans->where('id', $kec_id)->findAll();
+        $data["kecamatans"] = $kecamatans->where('id_kec', $kec_id)->findAll();
         $data["weeks"] = ["1 (Satu)", "2 (Dua)", "3 (Tiga)", "4 (Empat)", "5 (Lima)"];
         $data["months"] = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
-        $data["kecamatans"] = $kecamatans->findAll();
         $data["kelurahans"] = $kelurahans->where("id_kec", $kec_id)->findAll();
-
         return view('form.php', $data);
     }
     public function store()
